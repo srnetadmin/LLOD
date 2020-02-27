e83373604b2468a54841890b710f8ad5e49b3f8d
srnetadmin <ghuff@shadowreaver.net>
2020-02-27 12:33:53 -0600

    Add files via upload

---
+++ LLOD.ps1
@@ -0,0 +1,6 @@
+﻿$Users = New-Object System.Collections.ArrayList
+$Group = Read-Host "Please enter the AD Group you want to review"
+$Users = Get-ADGroupMember $Group |select name
+    foreach ($User in $Users)
+{        get-ADUser $User.name -Properties * | Select-Object Name, LastLogonDate
+        }
